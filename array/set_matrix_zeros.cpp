dff6a33c5c83425309d9adb12ea56e0a0fa0042a
Sharvarivkale <1332sharvarikale@gmail.com>
2026-06-28 13:34:03 +0530

    brute force

---
+++ array/set_matrix_zeros.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+       int row_num =matrix.size();
+       int col_num=matrix[0].size();
+
+       int row[row_num]={0};
+       int col[col_num]={0};
+
+       for(int i=0;i<row_num;i++){
+        for(int j=0;j<col_num;j++){
+          if(matrix[i][j]==0){
+            row[i]=1;
+            col[i]=1;
+          }
+        }
+       }
+
+       for(int i=0;i<row_num;i++){
+        for(int j=0;j<col_num;j++){
+          if(row[i]==1||col[j]==1){
+            matrix[i][j]=0;
+          }
+        }
+       }
+        
+    }
+};
