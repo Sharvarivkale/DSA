50b92bc34d5dca1ce169fe6ef5647f754f5a5c8a
Sharvarivkale <1332sharvarikale@gmail.com>
2026-07-08 20:06:52 +0530

    reverse string

--- stack/reverse_string.cpp
+++ stack/reverse_string.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
 
   stack<char>s;
-  string str=sharvari;
+  string str="sharvari";
 
   for(int i=0;i<str.length();i++){
     char ch =str[i];
@@ -12,11 +12,11 @@ int main(){
   }
 
   string ans="";
-  while(!s.empty){
+  while(!s.empty()){
     char ch=s.top();
     ans.push_back(ch);
     s.pop();
   }
-  cout<<"reverse stack are"<<ans<<endl;
+  cout<<"reverse stack are: "<<ans<<endl;
   return 0;
 }
