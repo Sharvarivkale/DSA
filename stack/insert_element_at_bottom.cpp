f0b14dfad53baa081b85cad18fc1854732c01f9c
Sharvarivkale <1332sharvarikale@gmail.com>
2026-07-09 13:26:03 +0530

    valid parenthesis

---
+++ stack/insert_element_at_bottom.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+   void solve(stack<int> &st, int x){
+       if(st.empty()){
+           st.push(x);
+           return;
+       }
+       int num=st.top();
+       st.pop();
+       
+       solve(st,x);
+       
+       st.push(num);
+   }
+    stack<int> insertAtBottom(stack<int> st, int x) {
+        solve(st,x);
+        return st;
+    }
+};
