6928550d2b0cbb4ec1b11c5640c872922fa2c411
Sharvarivkale <1332sharvarikale@gmail.com>
2026-07-22 14:52:35 +0530

    binary search impelementation

--- binary search/bs_implemntation_basic_concept.cpp
+++ binary search/bs_implemntation_basic_concept.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int binarysearch(vector<int> &nums,int target){
 int start=0;
 int end=nums.size()-1;
+int mid=start+(end-start)/2;
 
 while(start<=end){
-  int mid=start+(end-start)/2;
+
 
   if(nums[mid]==target){
     return mid;
@@ -19,6 +20,7 @@ while(start<=end){
   else{
    start=mid+1;
   }
+   mid=start+(end-start)/2;
 }
 return -1;
   
@@ -26,7 +28,7 @@ return -1;
 int main(){
   vector<int> arr={1,2,3,4,5,8,9};
 
-  int ans=binarysearch(arr,5);
+  int ans=binarysearch(arr,9);
 
   if(ans != -1){
     cout<<"element found at index :"<<ans<<endl;
