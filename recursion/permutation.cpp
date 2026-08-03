c80fe297c8233ef1698e7dfdc00c8794d7a22725
Sharvarivkale <1332sharvarikale@gmail.com>
2026-08-03 15:08:38 +0530

    permutataion of number

--- recursion/permutation.cpp
+++ recursion/permutation.cpp
@@ -1,17 +1,18 @@
 class Solution {
-     void solve(vector<int>& nums,vector<vector<int>> &ans,int index){
-       if(index>=nums.size()){
-         ans.push_back(nums);
-         return;
-       }
-       for(int i =index;i<nums.size();i++){
-        swap(nums[i],nums[index]);
-        solve(nums,ans,index+1);
-        //backtrack
-        swap(nums[i],nums[index]);
-       }
-    }
 public:
+   void solve(vector<int>& nums,vector<vector<int>> &ans,int index){
+    if(index>=nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=index;i<nums.size();i++){
+      swap(nums[index],nums[i]);
+      solve(nums,ans,index+1);
+
+      //backtracking
+      swap(nums[i],nums[index]);
+    }
+   }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         int index=0;
