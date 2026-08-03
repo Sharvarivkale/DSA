c08f38e13624fc74e58ba050422a0a96df56c3cd
Sharvarivkale <1332sharvarikale@gmail.com>
2026-08-03 13:19:48 +0530

    subset creation (powersets)

---
+++ recursion/subset_creation_powerset.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void solve(vector<int> &nums,vector<vector<int>> &ans,vector<int> &output,int index){
+  //base condition
+  if(index>=nums.size()){
+    ans.push_back(output);
+    return;
+  }
+  //exclude
+  solve(nums,ans,output,index+1);
+
+  //include
+  output.push_back(nums[index]);
+  solve(nums,ans,output,index+1);
+
+  //pop it out
+  output.pop_back();
+}
+
+vector<vector<int>> subsets(vector<int> &nums){
+  vector<vector<int>> ans;
+  vector<int> output;
+  int index=0;
+  solve(nums,ans,output,index);
+  return ans;
+
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+
+    vector<vector<int>> result = subsets(nums);
+
+    cout << "All Subsets are:\n";
+
+    for (auto subset : result) {
+        cout << "{ ";
+        for (int x : subset) {
+            cout << x << " ";
+        }
+        cout << "}" << endl;
+    }
+
+    return 0;
+}
