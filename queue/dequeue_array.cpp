be78c98413cbb2b48f86ca8e398d50c5cb263c1f
Sharvarivkale <1332sharvarikale@gmail.com>
2026-08-07 23:57:09 +0530

    queue_implemntation_array_dequeue kiya

--- queue/dequeue_array.cpp
+++ queue/dequeue_array.cpp
@@ -5,6 +5,19 @@ using namespace std;
 int front=0;
 int rear=0;
 
+bool isfull(int *arr,int size){
+  if(front==0 && rear==size){
+    return true;
+  }
+  return false;
+}
+bool isempty(int *arr){
+  if(front==rear){
+    return true;
+  }
+  return false;
+}
+
 void enqueue(int data,int size,int *arr){
   if(rear==size && front==0){
     cout<<"bhai queue full hai"<<endl;
@@ -31,30 +44,65 @@ void dequeue(int *arr){
   }
 }
 
-
-
-
 int main(){
   int size=6;
   int *arr=new int[size];
+
+  if(isfull(arr,size)){
+    cout<<"behen ye full hai arr kuch add mat kar !!"<<endl;
+  }
+  else{
+    cout<<"kuch to hai yaha pe or empty hai!!"<<endl;
+  }
+
+
   enqueue(5, size, arr);
   enqueue(8, size, arr);
+  enqueue(7, size, arr);
+  enqueue(6, size, arr);
   enqueue(7, size, arr);
   enqueue(6, size, arr);
 
   cout << "Queue elements before enqueue are:" << endl;
 
-  for(int i=0;i<rear;i++){
-    cout<<arr[i]<<endl;
-  }
-  cout << "Queue elements after enqueue are:" << endl;
-  dequeue(arr);
-  dequeue(arr);
-  dequeue(arr);
-  dequeue(arr);
 
   for(int i=0;i<rear;i++){
     cout<<arr[i]<<endl;
+  }
+    if(isfull(arr,size)){
+    cout<<"behen ye full hai arr kuch add mat kar !!"<<endl;
+  }
+  else{
+    cout<<"kuch to hai yaha pe or empty hai!!"<<endl;
+  }
+
+  cout << "Queue elements after enqueue are:" << endl;
+  dequeue(arr);
+  dequeue(arr);
+ 
+   for(int i=0;i<rear;i++){
+    cout<<arr[i]<<endl;
+  }
+
+    if(isempty(arr)){
+    cout<<"behen ye null hai arr kuch add kar !!"<<endl;
+  }
+  else{
+    cout<<"kuch to hai yaha pe or full hai!!"<<endl;
+  }
+  dequeue(arr);
+  dequeue(arr);
+  dequeue(arr);
+  dequeue(arr);
+  for(int i=0;i<rear;i++){
+    cout<<arr[i]<<endl;
+  }
+
+  if(isempty(arr)){
+    cout<<"behen ye null hai arr kuch add kar !!"<<endl;
+  }
+  else{
+    cout<<"kuch to hai yaha pe or full hai!!"<<endl;
   }
   cout<<front<<" "<<rear<<endl;
   return 0;
